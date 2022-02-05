d3cbcdd39be6843286a26234888ff4a98c357d48
AarumugaPandi <95572288+AarumugaPandi@users.noreply.github.com>
2022-02-05 17:26:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import styles from './styles.module.css'
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+  return( 
+  <div className={styles.test}>Example Component: {text}</div>
+)}
